fc9acf28f042443007d44326d185d1cbd6816533
Adam Kadlec <adam.kadlec@gmail.com>
2020-04-16 15:00:52 +0200

    Cleaning

--- src/Controllers/AuthenticateV1Controller.php
+++ src/Controllers/AuthenticateV1Controller.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types = 1);
 
 /**
- * BaseV1Controller.php
+ * AuthenticateV1Controller.php
  *
  * @license        More in license.md
  * @copyright      https://www.fastybird.com
